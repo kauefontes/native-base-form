7c4c42db7858803d03518908eaa8673c3aa2dd48
Kaue Pereira <kauefontes@outlook.com>
2022-10-20 20:57:20 -0400

    Added support for FormControl to Input Component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,29 @@
-import { Input as NativeBaseInput, IInputProps } from 'native-base'
+import { Input as NativeBaseInput, IInputProps, FormControl } from 'native-base'
 
-export function Input({ ...rest }: IInputProps) {
+type Props = IInputProps & {
+    errorMessage?: string | null
+}
+
+export function Input({ errorMessage = null, isInvalid, ...rest }: Props) {
+    const invalid = !!errorMessage || isInvalid
     return (
-        <NativeBaseInput
-            backgroundColor="gray.700"
-            fontSize="md"
-            height={16}
-            marginBottom={4}
-            _focus={{
-                backgroundColor: "gray.200",
-                borderWidth: 2,
-                borderColor: "green.500"
-            }}
-            {...rest}
-        />
+        <FormControl marginBottom={4} isInvalid={invalid}>
+            <NativeBaseInput
+                backgroundColor="gray.700"
+                fontSize="md"
+                height={16}
+                isInvalid={invalid}
+                _focus={{
+                    backgroundColor: "gray.200",
+                    borderWidth: 2,
+                    borderColor: "green.500"
+                }}
+                _invalid={{
+                    borderWidth: 2,
+                    borderColor: "red.500"
+                }}
+                {...rest}
+            />
+        </FormControl>
     )
 }
