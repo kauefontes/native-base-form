82017f7e346b1257ca3fed6b07930e65d5c03fb9
Kaue Pereira <kauefontes@outlook.com>
2022-10-20 20:41:33 -0400

    Added Inputs to signup screen

--- src/Screens/SignUp.tsx
+++ src/Screens/SignUp.tsx
@@ -1,18 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Center, Heading, VStack } from 'native-base';
+import { Input } from '../components/Input';
 
 export function SignUp() {
     return (
-        <View style={styles.container}>
-            <Text>Sign up</Text>
-        </View>
+        <VStack bgColor='gray.300' flex={1} paddingX={10}>
+            <Center>
+                <Heading marginY={24}>
+                    Sign up
+                </Heading>
+
+                <Input placeholder='Name' />
+                <Input placeholder='E-mail' />
+                <Input placeholder='Password' />
+                <Input placeholder='Confirm Password' />
+            </Center>
+        </VStack>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
