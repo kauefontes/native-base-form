7c91925a9e7e78fd2b2fcd99feafb8275f3f014a
Kaue Pereira <kauefontes@outlook.com>
2022-10-20 21:13:28 -0400

    Added FormControl to the form

--- src/Screens/SignUp.tsx
+++ src/Screens/SignUp.tsx
@@ -1,7 +1,14 @@
-import { Center, Heading, VStack } from 'native-base';
-import { Input } from '../components/Input';
+import { Center, Heading, VStack } from 'native-base'
+import { Input } from '../components/Input'
+import { Button } from '../components/Button'
+import { useForm, Controller } from 'react-hook-form'
 
 export function SignUp() {
+    const { control, handleSubmit } = useForm()
+
+    function handleSignup(data: any) {
+        console.log(data)
+    }
     return (
         <VStack bgColor='gray.300' flex={1} paddingX={10}>
             <Center>
@@ -9,10 +16,49 @@ export function SignUp() {
                     Sign up
                 </Heading>
 
-                <Input placeholder='Name' />
-                <Input placeholder='E-mail' />
-                <Input placeholder='Password' />
-                <Input placeholder='Confirm Password' />
+                <Controller
+                    control={control}
+                    name="name"
+                    render={({ field: { onChange } }) => (
+                        <Input
+                            placeholder='Name'
+                            onChangeText={onChange}
+                        />
+                    )}
+                />
+                <Controller
+                    control={control}
+                    name="email"
+                    render={({ field: { onChange } }) => (
+                        <Input
+                            placeholder='E-mail'
+                            onChangeText={onChange}
+                        />
+                    )}
+                />
+                <Controller
+                    control={control}
+                    name="password"
+                    render={({ field: { onChange } }) => (
+                        <Input
+                            placeholder='Password'
+                            secureTextEntry
+                            onChangeText={onChange}
+                        />
+                    )}
+                />
+                <Controller
+                    control={control}
+                    name="password_confirm"
+                    render={({ field: { onChange } }) => (
+                        <Input
+                            placeholder='Password Confirm'
+                            secureTextEntry
+                            onChangeText={onChange}
+                        />
+                    )}
+                />
+                <Button title='Submit' onPress={handleSubmit(handleSignup)} />
             </Center>
         </VStack>
     );
